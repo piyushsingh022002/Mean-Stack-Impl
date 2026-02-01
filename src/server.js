72771e5f5cad16145066966dbb627aa5ce3d8cb2
Piyush Singh <piyush@centroxy.com>
2026-02-01 16:36:32 +0530

    env file added, mongoose installed, db connection set

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import notesRoutes from "./Routes/notesRoutes.js"
 // const express = require("express");
+import connectDB from "./configs/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-
+// console.log(process.env.MONGO_URI);
 const app = express();
-
+ connectDB();
 //whenever request comes from an url, which starts from /api/notes, it will be handled by notesRoutes
 app.use("/api/notes", notesRoutes);
 
